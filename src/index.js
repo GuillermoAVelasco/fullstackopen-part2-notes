358a7b5ddee519175ef70876b74119fe957203df
Guillermo Velasco <alfadoble10@gmail.com>
2021-08-23 15:18:38 -0300

    Agregando state, y funcionalidades a notes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
-import reportWebVitals from './reportWebVitals';
 
 const notes = [
   {
@@ -24,15 +23,7 @@ const notes = [
   }
 ]
 
-
-
-
 ReactDOM.render(
      <App notes={notes} />,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
